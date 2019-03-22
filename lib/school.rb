4111bc4d0d0303a5d9009f54fcfad2c35e3de677
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-03-22 08:38:52 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+attr_accessor :roster, :name
+  def initialize(name)
+    @name =name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(student)
+    roster[student]
+  end
+
+  def sort
+    new_hash = {}
+    roster.each do |grade, student|
+      new_hash[grade] = student.sort
+    end
+    new_hash
+  end
+
+  end
+
+# school = School.new("Bayside High School")
